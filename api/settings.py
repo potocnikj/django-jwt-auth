f059d495de11ab1b7e6cb1accb103b783e0fdc45
Jan Potočnik <jan.potocnik@pop-tv.si>
2018-07-10 10:07:11 +0200

    json format is default

--- api/settings.py
+++ api/settings.py
@@ -124,11 +124,5 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     # When you enable API versioning, the request.version attribute will contain a string
     # that corresponds to the version requested in the incoming client request.
-    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.URLPathVersioning',
-    'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
-    ),
-    'DEFAULT_PARSER_CLASSES': (
-        'rest_framework.parsers.JSONParser',
-    )
+    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.URLPathVersioning'
 }
